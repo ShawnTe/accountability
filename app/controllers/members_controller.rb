457cc7872a8acc0d7a25b11e0f3dbfeda2b2401d
shawn <shawn@naturalprofessional.com>
2016-09-23 15:07:59 -0700

    Move new member form to groups show page. Route not working

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,10 +2,18 @@ class MembersController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+    
+  end
+
   def show
     @member = Member.find(params[:mem_id])
     @goals = @member.goals
-    p @member + "In the Members show route"
+    p @member
+    p "* " * 50
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   resources :groups do
     resources :members
-    get 'groups/:id/members/:id', to: 'members#show'
   end
+  get '/members/new', to: 'members#new'
+  # post '/members/create', to: 'members#create'
+
+  # get '/members/create', to: 'members'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
