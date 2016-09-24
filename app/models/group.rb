9cbff2b2a39c392fe6f712d7580dae73a34b5955
shawn <shawn@naturalprofessional.com>
2016-09-24 12:28:07 -0700

    Add private methods to ignore empty empty records

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,12 @@
 class Group < ApplicationRecord
   has_many :members, :dependent => :destroy
 
-  accepts_nested_attributes_for :members, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :members, reject_if: :members_rejectable?, :allow_destroy => true
+
+private
+
+def members_rejectable?(attribute)
+  attribute['name'].blank? && new_record?
+end
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,10 @@ class Member < ApplicationRecord
   has_many :goals, :dependent => :destroy
   belongs_to :group
 
-  accepts_nested_attributes_for :goals, :reject_if => lambda { |a| a[:description].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :goals, reject_if: :goals_rejectable?, :allow_destroy => true
+
+private
+  def goals_rejectable?(attribute)
+    attribute['description'].blank? && new_record?
+  end
 end
